6f525860d69ceb34c6f5ce233738efb54eda667f
genecarles <genecarles@gmail.com>
2026-07-03 20:57:05 +0200

    ssss

--- components/share.js
+++ components/share.js
@@ -63,9 +63,8 @@ display: inline-block;
 
     <!-- DIV 2 — fons fix amb icones -->
     <div class="share-fons">
-        <a href="mailto:${CONFIG.EMAIL}"><img src="${CONFIG.ASSETS}icon/Icomail.png" alt="Instagram" class="share-icona"></a>
-        <a href="#">IG</a>
-        <a href="#">G</a>
+        <a href="mailto:${CONFIG.EMAIL}"><img src="${CONFIG.ASSETS}icon/Icomail.png" alt="Mail" class="share-icona"></a>
+        <a href="${CONFIG.INSTAGRAM}" target="_blank"><img src="${CONFIG.ASSETS}icon/Icoinsta.png" alt="Instagram" class="share-icona"></a>
     </div>
 
     <!-- DIV 1 — cortina de color -->
