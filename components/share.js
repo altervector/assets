77f2a843a285c0fcf279d18fa12fe0304d0e3a60
genecarles <genecarles@gmail.com>
2026-07-03 18:57:17 +0200

    adf

--- components/share.js
+++ components/share.js
@@ -56,6 +56,8 @@ class ShareBtn extends HTMLElement {
 
     <!-- DIV 2 — fons fix amb icones -->
     <div class="share-fons">
+                        <a href="mailto:${window.CONFIG.EMAIL}"><img src="${window.CONFIG.ASSETS}icon/Icomail.png" alt="Instagram" class="icona-app"> ${CONFIG.EMAIL}</a>
+
         <a href="#">IG</a>
         <a href="#">G</a>
     </div>
